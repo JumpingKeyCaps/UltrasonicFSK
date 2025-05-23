203d14d2cf695cffcb8b473e2fd14124fcf8c024
Samuel Lebailly <sam.meusa@gmail.com>
2025-05-23 16:28:49 +0200

    feature(main): Update the main activity to use the navigator.

--- app/src/main/java/com/lebaillyapp/ultrasonicfsk/MainActivity.kt
+++ app/src/main/java/com/lebaillyapp/ultrasonicfsk/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.lebaillyapp.ultrasonicfsk.navigation.AppNavigator
 import com.lebaillyapp.ultrasonicfsk.ui.theme.UltrasonicfskTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             UltrasonicfskTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AppNavigator()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    UltrasonicfskTheme {
-        Greeting("Android")
-    }
-}
