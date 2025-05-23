839d14978d9275c7493949087522ee4063588c3a
Samuel Lebailly <sam.meusa@gmail.com>
2025-05-23 17:14:28 +0200

    feature(data/repository): Create a wireframe of the Receiver Viewmodel.

---
+++ app/src/main/java/com/lebaillyapp/ultrasonicfsk/viewmodel/ReceiverViewModel.kt
@@ -0,0 +1,24 @@
+package com.lebaillyapp.ultrasonicfsk.viewmodel
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import com.lebaillyapp.ultrasonicfsk.data.repository.FskRepository
+
+class ReceiverViewModel(
+    private val repository: FskRepository = FskRepository()
+) : ViewModel() {
+
+    fun init(context: Context) {
+        repository.initDecoder(context)
+    }
+
+    fun startDecoding() {
+        repository.startDecoding()
+    }
+
+    fun stopDecoding() {
+        repository.stopDecoding()
+    }
+
+    val bitFlow = repository.bitFlow
+}
