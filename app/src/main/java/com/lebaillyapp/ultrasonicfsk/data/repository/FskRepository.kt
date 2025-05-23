4e2f72942f12b90c578912bfe80c2e429854aedf
Samuel Lebailly <sam.meusa@gmail.com>
2025-05-23 17:14:04 +0200

    feature(data/repository): Upgrade the FSK repository to use the fsk decoder and the fsk encoder.

--- app/src/main/java/com/lebaillyapp/ultrasonicfsk/data/repository/FskRepository.kt
+++ app/src/main/java/com/lebaillyapp/ultrasonicfsk/data/repository/FskRepository.kt
@@ -1,18 +1,38 @@
 package com.lebaillyapp.ultrasonicfsk.data.repository
 
+import android.content.Context
+import com.lebaillyapp.ultrasonicfsk.data.service.FskDecoder
 import com.lebaillyapp.ultrasonicfsk.data.service.FskEncoder
+import kotlinx.coroutines.flow.SharedFlow
 
 /**
- * Repository qui encapsule la logique d'encodage FSK.
+ * Repository qui encapsule la logique d'encodage/decodage FSK.
  * Peut être utilisé par un ViewModel ou une autre couche de l'app.
  */
 class FskRepository(
-    private val encoder: FskEncoder = FskEncoder()
+    private val encoder: FskEncoder = FskEncoder(),
+    private var decoder: FskDecoder? = null
 ) {
+
     /**
-     * Encode un message texte en une séquence de fréquences FSK.
+     * Initialise le décodeur avec un contexte si non déjà initialisé.
      */
+    fun initDecoder(context: Context) {
+        if (decoder == null) decoder = FskDecoder(context)
+    }
+
     fun encodeMessage(message: String): List<Pair<Double, Long>> {
         return encoder.encode(message)
+    }
+
+    val bitFlow: SharedFlow<Int>?
+        get() = decoder?.bitFlow
+
+    fun startDecoding() {
+        decoder?.startDecoding()
+    }
+
+    fun stopDecoding() {
+        decoder?.stopDecoding()
     }
 }
