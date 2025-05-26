cc88f2fce462194e6e618f605f8c43815f00070b
Samuel Lebailly <sam.meusa@gmail.com>
2025-05-26 22:15:00 +0200

    feature(navigation): Add tone test screen to the navigation graph.

--- app/src/main/java/com/lebaillyapp/ultrasonicfsk/navigation/AppNavigator.kt
+++ app/src/main/java/com/lebaillyapp/ultrasonicfsk/navigation/AppNavigator.kt
@@ -6,12 +6,14 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.lebaillyapp.ultrasonicfsk.screen.ReceiverScreen
 import com.lebaillyapp.ultrasonicfsk.screen.SenderScreen
+import com.lebaillyapp.ultrasonicfsk.screen.ToneTestScreen
 
 @Composable
 fun AppNavigator() {
     val navController = rememberNavController()
-    NavHost(navController, startDestination = "sender") {
+    NavHost(navController, startDestination = "toneTest") {
         composable("sender") { SenderScreen(navController) }
         composable("receiver") { ReceiverScreen(navController) }
+        composable("toneTest") { ToneTestScreen(navController) }
     }
 }
