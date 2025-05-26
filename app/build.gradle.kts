5e52e31b80558f7ced7b17b43dc0cf445c5de2b7
Samuel Lebailly <sam.meusa@gmail.com>
2025-05-26 18:07:23 +0200

    fix(build.gradle): fix navigation-compose dependency.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,6 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.androidx.navigation.compose.jvmstubs)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -57,6 +56,9 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation ("androidx.navigation:navigation-compose:2.9.0")
+
 
     implementation("com.google.accompanist:accompanist-permissions:0.34.0")
     // FFT library
